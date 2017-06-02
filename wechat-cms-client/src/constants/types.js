3171bdebdaf4d041b377f99bdd2a07bb1686769c
wind634 <2911184332@qq.com>
2017-06-02 10:34:17 +0800

    新增搜索菜单

--- wechat-cms-client/src/constants/types.js
+++ wechat-cms-client/src/constants/types.js
@@ -18,3 +18,10 @@ export const loginingActionType = 'LOGINING'
 
 export const logoutActionType = 'LOGOUT'
 export const logoutingActionType = 'LOGOUTING'
+
+
+// 用户列表相关
+export const getUserListSuccessActionType = 'GET_USERLIST_SUCCESS'
+export const getUserListFailActionType = 'GET_USERLIST_FAIL'
+export const getUserListErrorActionType = 'GET_USERLIST_ERROR'
+
