b056ccbad31941709bf9763198601171e93430a7
wind634 <2911184332@qq.com>
2017-06-02 10:43:11 +0800

    新增搜索菜单

--- wechat-cms-client/src/reducers/userReducer.js
+++ wechat-cms-client/src/reducers/userReducer.js
@@ -1,3 +1,31 @@
 /**
- * Created by wangjiang on 17/6/2.
+ * Created by wangjiang on 17/5/31.
  */
+
+import { getUserListSuccessActionType, getUserListFailActionType, getUserListErrorActionType, gettingUserListActionType}  from '../constants/types';
+
+//reducer
+const initialState = {
+    // 获取用户列表相关
+    data: [],
+    pagination: {},
+    loading: false,
+}
+
+const userReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case getUserListSuccessActionType:
+            return Object.assign({}, state, {data: action.payload.data, pagination:action.payload.pagination, loading:false})
+        case getUserListFailActionType:
+            // 不修改原来的state对象返回一个新的state对象
+            return Object.assign({}, state, {loading:false})
+        case getUserListErrorActionType:
+            return Object.assign({}, state, {loading: false})
+        case gettingUserListActionType:
+            return Object.assign({}, state, {loading: true})
+        default:
+            return initialState
+    }
+}
+
+export default {initialState, userReducer}
